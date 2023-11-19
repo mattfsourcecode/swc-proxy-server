71dda351a407a7ab7d18e6e79a6f67518f051472
matt-development-work <24415914+matt-development-work@users.noreply.github.com>
2023-11-18 22:14:42 -0800

    Improve error handling

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ app.use("/api", proxy);
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send(err.message);
 });
 
 // Start the server.
